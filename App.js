3f8e506b8972ecbe72728ed415ecd5af646eb96e
dixmellow <ingrid.melo1b3@gmail.com>
2024-03-04 12:05:12 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ingrid Mello</Text>
+      <Text>Kaue Açougueiro</Text>
+      <Text>Clone do projeto aula 04-03-2024 lado A</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
