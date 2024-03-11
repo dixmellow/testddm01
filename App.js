4c8d3c44d4579ed739adc765bcd971fab819a38d
dixmellow <ingrid.melo1b3@gmail.com>
2024-03-11 11:53:57 -0300

    isenrindo Botão

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ingrid Mello</Text>
+      <Text>Gusta Repositor</Text>
       <Text>Kaue Açougueiro</Text>
       <Text>Clone do projeto aula 04-03-2024 lado A</Text>
+      <Button title='Enviar'></Button>
       <StatusBar style="auto" />
     </View>
   );
